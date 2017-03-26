928c2c27decfe1b1ddc663c41584a76929634b43
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-03-25 23:20:40 -0500

    Initial commit

---
+++ bot/API_Async_MultiThreading.js
@@ -0,0 +1,59 @@
+var async = require('async');
+var request = require('request');
+
+
+var quotes_data="";
+//http://stackoverflow.com/questions/33111961/typeerror-task-is-not-a-function-in-async-js-parrallel
+// without function (callback){Image(callback)}, was getting error:Task is not a function
+async.parallel([
+    function (callback){Image(callback)},
+    function (callback){quotes(callback)}
+], function(err, data){
+    if (err) {
+        console.log(err);
+    } else {
+        // data is an array of results from each function
+        console.log('\n** Parallel Results');
+
+        console.log('Your image url is ' + data[0]);  // an *array* of results,
+        console.log('Your quotes for the day is' + data[1].quotes); // an *array* of results,
+    }
+});
+
+
+function Image(callback)
+{
+    var Pixabay_key=process.env.PIXABAY_API_KEY;
+    var url = "https://pixabay.com/api/"; // the base URL
+    var params = { 'key':Pixabay_key, 'q' :'beautiful', 'image_type':'photo'  };
+    request({url:url, qs: params}, function(err, res, data)
+    {
+        if(err)
+        {
+            callback("Error !!! ")
+        }
+        else {
+            var Image = JSON.parse(data);
+            if (err)
+            {
+                return callback(null, 'No Image found');
+            }
+            callback("The url for the Image is " + Image['userImageURL']);
+        }
+    });
+
+}
+
+
+function quotes(callback)
+{
+    var url = 'http://quotes.rest/qod.json';
+    request(url, function(err, res, data){
+        var quotes = JSON.parse(data);
+        if (quotes.status == '404')
+        { return callback(null, 'error');
+        }
+        quotes_data = {'quotes':quotes.quotes, 'author':quotes.author, 'copyright':quotes.copyright};
+        callback( quotes_data)
+    })
+}
---
+++ bot/twitter.js
@@ -0,0 +1,54 @@
+var Twitter = require('twitter');
+var fs = require('fs');
+
+function post(status, image, callback) {
+
+    var client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+
+    try {
+        img = fs.readFileSync(image);
+    } catch (e) {
+        console.log('Error reading image file');
+        return callback(e);
+    }
+
+    client.post('media/upload', {media:img}, function(error, media, response){
+
+        // media object returned, which is something that can be tweeted,
+
+        if (error) {
+            console.log('error creating media ' + error);
+            return callback(error);
+        }
+
+        else {
+
+            var statusText = {
+                status: status,
+                media_ids: media.media_id_string
+            };
+
+            client.post('statuses/update', statusText, function(error, tweet, response) {
+
+                if (error) {
+                    console.log('error tweeting ' + error);
+                    return callback(error)
+                }
+
+                else {
+                    console.log('Tweeted ' + tweet.text);
+                    return callback(null, tweet)
+                }
+
+            });
+        }
+    });
+}
+
+
+module.exports = post;
