cfc24a4375ba3cb02141d9cbe13608d3f0e06262
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-04-18 13:26:59 -0500

    Documentation done;Twitter runs once a day; Checked for errors;Completed;

--- bot/API_Async.js
+++ bot/API_Async.js
@@ -21,48 +21,48 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE*/
 
-
+// this is just to check if
 var async = require('async');
 var request = require('request');
-//var image_downloader = require('image-downloader');
+var image_downloader = require('image-downloader');
 var fs = require('fs');
 const path = require('path');
 
 var quotes_data="";
 var Image_data="";
 //http://stackoverflow.com/questions/33111961/typeerror-task-is-not-a-function-in-async-js-parrallel
-// without function (callback){Image(callback)}, was getting error:Task is not a function
-// async.parallel([
-//     Image,
-//     quotes
-//     ],
-//     function(err, data)
-//     {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             // data is an array of results from each function
-//             console.log('\n** Parallel Results');
-//
-//             console.log('Your image url is ' + data[0].Image);  // an *array* of results,
-//             console.log('Your quotes for the day is :- ' + data[1].quotes); // an *array* of results,
-//         }
-//         //
-//         var options =
-//         {
-//             url: data[0].Image,
-//             dest: '../public/images/Downloaded_Image.jpg',
-//             done: function (err, filename, Image)
-//             {
-//                 if (err) {
-//                     throw err;
-//                 }
-//                 console.log('File saved to', filename);
-//
-//             }
-//         };
-//         image_downloader(options);
-//     });
+//without function (callback){Image(callback)}, was getting error:Task is not a function
+async.parallel([
+    Image,
+    quotes
+    ],
+    function(err, data)
+    {
+        if (err) {
+            console.log(err);
+        } else {
+            // data is an array of results from each function
+            console.log('\n** Parallel Results');
+
+            console.log('Your image url is ' + data[0].Image);  // an *array* of results,
+            console.log('Your quotes for the day is :- ' + data[1].quotes); // an *array* of results,
+        }
+        //
+        var options =
+        {
+            url: data[0].Image,
+            dest: '../public/images/Downloaded_Image.jpg',
+            done: function (err, filename, Image)
+            {
+                if (err) {
+                    throw err;
+                }
+                console.log('File saved to', filename);
+
+            }
+        };
+        image_downloader(options);
+    });
 // This makes an API calls to Pixabay to fetch images based on some hard-coded search params;
 function Image(callback)
 {
@@ -101,8 +101,7 @@ function quotes(callback)
     request(url, function(err, res, data)
     {
         var quotes_object = JSON.parse(data);
-        //console.log(data);
-        if (quotes.status == '404')
+        if (quotes_object.status == '404')
         { return callback(null, 'error');
         }
         //returns quotes and author's name
@@ -110,7 +109,7 @@ function quotes(callback)
         callback(null, quotes_data)
     })
 }
-
+//creating objects ;
 module.exports =
 {
     Image:Image,
